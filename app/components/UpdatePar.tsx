f24f26dc5d0e118c0168e61febeb13b81f0850b2
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-09-27 19:55:29 -0400

    logo

--- app/components/UpdatePar.tsx
+++ app/components/UpdatePar.tsx
@@ -1,15 +1,15 @@
-import 'server-only'
-import prisma from '../../prisma/prisma'
+// import 'server-only'
+// import prisma from '../../prisma/prisma'
 
-export const UpdatePar = ({ id, par }: {id: number, par: number}) => {
- prisma.item.update({
-    where: {
-      id: id
-    },
-    data: {
-      par: par
-    }
-  })
-  return null
-}
+// export const UpdatePar = ({ id, par }: {id: number, par: number}) => {
+//  prisma.item.update({
+//     where: {
+//       id: id
+//     },
+//     data: {
+//       par: par
+//     }
+//   })
+//   return null
+// }
 
